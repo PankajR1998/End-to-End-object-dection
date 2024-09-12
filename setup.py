b0681c9928e485f39709bb12c03c3097e5d7a793
Pankaj <rawatpankaj9919@gmail.com>
2024-09-12 11:43:06 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup( # this setup function will look for every constructor file and install that folder.
+    name="SignLanguage",
+    version="0.0.0",
+    author="PankajRawat",
+    author_email="rawatpankaj9919@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
